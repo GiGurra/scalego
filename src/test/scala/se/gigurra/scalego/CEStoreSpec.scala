194642d514706dc9f220f7e87df341cb14a0691d
Johan Kjölhede <gigurra@gmail.com>
2016-09-17 11:46:05 +0200

    Add some tests

--- src/test/scala/se/gigurra/scalego/CEStoreSpec.scala
+++ src/test/scala/se/gigurra/scalego/CEStoreSpec.scala
@@ -11,32 +11,39 @@ class CEStoreSpec
   with Matchers
   with OneInstancePerTest {
 
-  class StoreTypes extends Types {
+  class StringBasedIdTypes extends Types {
     override type ComponentTypeId = String
     override type EntityId = String
     override type ProcessTime = Long
-    override type ProcessContext = CEStore[this.type]
+    override type ProcessContext = CEStore[StringBasedIdTypes]
+  }
+
+  class IntBasedIdTypes extends Types {
+    override type ComponentTypeId = Int
+    override type EntityId = Int
+    override type ProcessTime = Long
+    override type ProcessContext = CEStore[IntBasedIdTypes]
   }
 
   "CEStore" should {
 
     "Be created" in {
-      noException should be thrownBy (CEStore.Builder[StoreTypes]() build : CEStore[StoreTypes])
+      noException should be thrownBy (CEStore.Builder[StringBasedIdTypes]() build : CEStore[StringBasedIdTypes])
     }
 
     "Add a system" in {
-      val positionSystem = new CESystem(ComponentTypeInfo[(Int, Int), StoreTypes]("position"))
-      noException should be thrownBy (CEStore.Builder[StoreTypes]() + positionSystem build : CEStore[StoreTypes])
+      val positionSystem = new CESystem(ComponentTypeInfo[(Int, Int), StringBasedIdTypes]("position"))
+      noException should be thrownBy (CEStore.Builder[StringBasedIdTypes]() + positionSystem build : CEStore[StringBasedIdTypes])
     }
 
     "Get the system of a component type" in {
       case class Position(x: Int, y: Int)
       case class Velocity(x: Int, y: Int)
-      implicit val positionTypeInfo = ComponentTypeInfo[Position, StoreTypes]("position")
-      implicit val velocityTypeInfo = ComponentTypeInfo[Velocity, StoreTypes]("velocity")
+      implicit val positionTypeInfo = ComponentTypeInfo[Position, StringBasedIdTypes]("position")
+      implicit val velocityTypeInfo = ComponentTypeInfo[Velocity, StringBasedIdTypes]("velocity")
       val positionSystem = new CESystem(positionTypeInfo)
       val velocitySystem = new CESystem(velocityTypeInfo)
-      val store = CEStore.Builder[StoreTypes]() + positionSystem + velocitySystem build
+      val store = CEStore.Builder[StringBasedIdTypes]() + positionSystem + velocitySystem build
 
       store.system[Position] shouldBe positionSystem
       store.system[Velocity] shouldBe velocitySystem
@@ -46,12 +53,12 @@ class CEStoreSpec
     "Create entities in the store" in {
       case class Position(x: Int = 0, y: Int = 0)
       case class Velocity(x: Int = 0, y: Int = 0)
-      implicit val positionTypeInfo = ComponentTypeInfo[Position, StoreTypes]("position")
-      implicit val velocityTypeInfo = ComponentTypeInfo[Velocity, StoreTypes]("velocity")
+      implicit val positionTypeInfo = ComponentTypeInfo[Position, StringBasedIdTypes]("position")
+      implicit val velocityTypeInfo = ComponentTypeInfo[Velocity, StringBasedIdTypes]("velocity")
       implicit val positionSystem = new CESystem(positionTypeInfo)
       implicit val velocitySystem = new CESystem(velocityTypeInfo)
 
-      val store = CEStore.Builder[StoreTypes]() + positionSystem + velocitySystem build
+      val store = CEStore.Builder[StringBasedIdTypes]() + positionSystem + velocitySystem build
 
       val e1 = store.entityBuilder(entityId = "1") + Position(1, 2) + Velocity(3, 4) build
       val e2 = store.entityBuilder(entityId = "2") + Position(5, 6) + Velocity(7, 8) build
@@ -70,12 +77,12 @@ class CEStoreSpec
     "Delete entities from the entire store" in {
       case class Position(x: Int = 0, y: Int = 0)
       case class Velocity(x: Int = 0, y: Int = 0)
-      implicit val positionTypeInfo = ComponentTypeInfo[Position, StoreTypes]("position")
-      implicit val velocityTypeInfo = ComponentTypeInfo[Velocity, StoreTypes]("velocity")
+      implicit val positionTypeInfo = ComponentTypeInfo[Position, StringBasedIdTypes]("position")
+      implicit val velocityTypeInfo = ComponentTypeInfo[Velocity, StringBasedIdTypes]("velocity")
       implicit val positionSystem = new CESystem(positionTypeInfo)
       implicit val velocitySystem = new CESystem(velocityTypeInfo)
 
-     val store = CEStore.Builder[StoreTypes]() + positionSystem + velocitySystem build
+      val store = CEStore.Builder[StringBasedIdTypes]() + positionSystem + velocitySystem build
 
       val e1 = store.entityBuilder(entityId = "1") + Position(1, 2) + Velocity(3, 4) build
       val e2 = store.entityBuilder(entityId = "2") + Position(5, 6) + Velocity(7, 8) build
@@ -91,6 +98,34 @@ class CEStoreSpec
       a[EntityHasNoSuchComponent] should be thrownBy e1[Velocity]
       e2[Position] shouldBe Position(5,6)
       e2[Velocity] shouldBe Velocity(7,8)
+    }
+
+    "Support different types of entity and component type Ids" in {
+
+      case class Position(x: Int = 0, y: Int = 0)
+      case class Velocity(x: Int = 0, y: Int = 0)
+      implicit val positionTypeInfo = ComponentTypeInfo[Position, IntBasedIdTypes](1)
+      implicit val velocityTypeInfo = ComponentTypeInfo[Velocity, IntBasedIdTypes](2)
+      implicit val positionSystem = new CESystem(positionTypeInfo)
+      implicit val velocitySystem = new CESystem(velocityTypeInfo)
+
+      val store = CEStore.Builder[IntBasedIdTypes]() + positionSystem + velocitySystem build
+
+      val e1 = store.entityBuilder(entityId = 1) + Position(1, 2) + Velocity(3, 4) build
+      val e2 = store.entityBuilder(entityId = 2) + Position(5, 6) + Velocity(7, 8) build
+
+      store -= 1
+
+      store.system[Position].size shouldBe 1
+      store.system[Velocity].size shouldBe 1
+      store.containsEntity(1) shouldBe false
+      store.containsEntity(2) shouldBe true
+
+      a[EntityHasNoSuchComponent] should be thrownBy e1[Position]
+      a[EntityHasNoSuchComponent] should be thrownBy e1[Velocity]
+      e2[Position] shouldBe Position(5,6)
+      e2[Velocity] shouldBe Velocity(7,8)
+
     }
   }
 }
