5ef71388b925dd39222b48eae3dbcbc909fa863b
Johan Kjölhede <gigurra@gmail.com>
2016-09-17 14:55:27 +0200

    cleanup

--- src/main/scala/se/gigurra/scalego/ComponentTypeInfo.scala
+++ src/main/scala/se/gigurra/scalego/ComponentTypeInfo.scala
@@ -3,6 +3,4 @@ package se.gigurra.scalego
 /**
   * Created by johan on 2016-06-26.
   */
-case class ComponentTypeInfo[T, T_Types <: Types](id: T_Types#ComponentTypeId) {
-  type ComponentType = T
-}
+case class ComponentTypeInfo[ComponentType, T_Types <: Types](id: T_Types#ComponentTypeId)
